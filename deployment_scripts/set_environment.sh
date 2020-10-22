66e27fb153fc6a8bc63f22aff0622cdd469dedd3
kotharipiyush <52200654+kotharipiyush@users.noreply.github.com>
2020-10-22 15:51:14 +0100

    Update set_environment.sh

--- deployment_scripts/set_environment.sh
+++ deployment_scripts/set_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 export DEFAULT_LOCATION=uksouth
-export SIRMIONE_ALIAS=bpsirmione
-export LIMONE_ALIAS=bplimone
-export SCORPIO_ALIAS=bpscorpio
-export VIRGO_ALIAS=bpvirgo
-export LIBRA_ALIAS=bplibra
-export ZODIAC_ALIAS=bpzodiac
-export DB_ADMIN_USER=nick
+export SIRMIONE_ALIAS=pk2438sirmione
+export LIMONE_ALIAS=pk2438bplimone
+export SCORPIO_ALIAS=pk2438bpscorpio
+export VIRGO_ALIAS=pk2438virgo
+export LIBRA_ALIAS=pk2438libra
+export ZODIAC_ALIAS=pk2438zodiac
+export DB_ADMIN_USER=pk2438
 export AAD_DOMAIN=xekina.onmicrosoft.com
 export AAD_TENANTID=3bc03625-3a0a-48c5-8aa5-12f22e401fff
 export AAD_CLIENTID=ccee7608-940c-42d9-ba86-a2845ef3a808
